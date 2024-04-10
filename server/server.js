1df5270f60ea85afe0dccc3266bf5db64ed5fca6
fb0801 <fbhatti0801@gmail.com>
2024-04-10 18:24:59 +0100

    express route 1

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3001
 const app = express();
 
 
+
+app.get("/getRestaurants", (req, res) => {
+    
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`server is  up and listening ${port}`)
 });
