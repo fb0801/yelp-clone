477b82f96b854d03f94e8c53a57ad09342d9058a
fb0801 <fbhatti0801@gmail.com>
2024-04-23 18:37:14 +0100

    homepage layout 4

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RestaurantList = () => {
+  return (
+    <div className='list-group'>
+      <table className="table table-hover table-dark">
+        <thead>
+            <tr className="bg-primary"></tr>
+        </thead>
+      </table>
+    </div>
+  )
+}
+
+export default RestaurantList
