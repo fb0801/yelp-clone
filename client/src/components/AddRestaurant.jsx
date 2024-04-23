8a4da3ba3915c065e33594242b0035f243f58092
fb0801 <fbhatti0801@gmail.com>
2024-04-23 18:05:23 +0100

    homepage layout 3

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -6,11 +6,22 @@ const AddRestaurant = () => {
         <form action="">
             <div className="form-row">
                 <div className="col">
-                    <input type="text" className='form-control'/>
+                    <input type="text" className='form-control' placeholder='name'/>
                 </div>
                 <div className="col">
-                    <input type='text'/>
+                    <input className='form-control' type='text' placeholder='location'/>
                 </div>
+                <div className="col">
+                    <select className='custom-select my-1 mr-sm-2'>
+                        <option disabled>Price Range</option>
+                        <option value="1">$</option>
+                        <option value='2'>$$</option>
+                        <option value='3'>$$$</option>
+                        <option value="4">$$$$</option>
+                        <option value="5">$$$$$</option>
+                    </select>
+                </div>
+                <button className="btn btn-primary">Add</button>
             </div>
         </form>
       
