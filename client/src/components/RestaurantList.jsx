c0125ad08ff4f9e8ee3f6f28589b50f454e3c104
fb0801 <fbhatti0801@gmail.com>
2024-04-24 17:32:14 +0100

    homepage layout 5

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -5,8 +5,27 @@ const RestaurantList = () => {
     <div className='list-group'>
       <table className="table table-hover table-dark">
         <thead>
-            <tr className="bg-primary"></tr>
+            <tr className="bg-primary">
+              <th scope='col'>Restaurant</th>
+              <th scope='col'>Location</th>
+              <th scope='col'>Price Range</th>
+              <th scope='col'>Ratings</th>
+              <th scope='col'>Edit</th>
+              <th scope='col'>Delete</th>
+
+
+            </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>maccies</td>
+            <td>new york</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td><button className="btn btn-warning">Update</button></td>
+            <td><button className="btn btn-warning">Delete</button></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
