64423da7f6b67084a4674023a63492b73bc11814
fb0801 <fbhatti0801@gmail.com>
2024-04-29 18:04:50 +0100

    update restaurants 2

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useParams} from 'react-router-dom'
 
 
-const UpdateRestaurant = () => {
-    useParams()
+const UpdateRestaurant = (props) => {
+    const {id} = useParams()
+    const [name, setName] = useState("")
+    const [location, setLocation] = useState("")
+    const [priceRange, setPriceRange] = useState("")
+
+
   return (
     <div>
-      
+      <form action="">
+        <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input value ={name} onChange={e => setName(e.target.value)} type="text" id="name" className="form-control" />
+        </div>
+
+        <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input value ={location} onChange={e => setLocation(e.target.value)} type="text" id="location" className="form-control" />
+        </div>
+
+        <div className="form-group">
+            <label htmlFor="name">Price Range</label>
+            <input value ={priceRange} onChange={e => setPriceRange(e.target.value)} type="number" id="price_range" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-  )
+  )  
 }
 
 export default UpdateRestaurant
