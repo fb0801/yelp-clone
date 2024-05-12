bcd76961dba82fe8ac7f963755af381977259a65
fb0801 <fbhatti0801@gmail.com>
2024-05-12 16:59:53 +0100

    rating 2

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -5,12 +5,12 @@ const StarRating = ({rating}) => {
 
     for (let i = 1; i <=5; i++) {
         if (i <=rating) {
-            stars.push(<i class="fa-solid fa-star"></i>)
+            stars.push(<i class="fa-solid fa- text-warning"></i>)
         } else if (i === Math.ceil(rating) && !Number.isInteger(rating)){
-            stars.push(<i class="fa-solid fa-star-half-stroke"></i>)
+            stars.push(<i class="fa-solid fa-star-half-stroke text-warning"></i>)
         }
         else {
-            stars.push(<i class="fa-regular fa-star"></i>)
+            stars.push(<i class="fa-regular fa-star text-warning"></i>)
         }
     }
 
