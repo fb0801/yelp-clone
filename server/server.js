8c588fb454d9d4fbb5f820e008a238dfa6797c21
fb0801 <fbhatti0801@gmail.com>
2024-05-15 17:45:42 +0100

    review route 3

--- server/server.js
+++ server/server.js
@@ -11,25 +11,25 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-//get all rest
+// Get all Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-
-    try{
-        const results = await db.query("select * from restaurants")
-         console.log(results)
-    res.status(404).json({
-        status: 'success',
-        results: results.rows.length,
+    try {
+      //const results = await db.query("select * from restaurants");
+      const restaurantRatingsData = await db.query(
+        "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
+      );
+  
+      res.status(200).json({
+        status: "success",
+        results: restaurantRatingsData.rows.length,
         data: {
-            restaurants: results.rows,
+          restaurants: restaurantRatingsData.rows,
         },
-    })
-
-
+      });
     } catch (err) {
-        console.log(err)
+      console.log(err);
     }
-})
+  });
 
 //get a rest
 app.get("/api/v1/restaurants/:id", async (res,req) => {
